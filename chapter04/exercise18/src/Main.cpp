c12706d89fb2ca3baee3262acdcc628b41cac021
William Jin <wjin0702@yahoo.com>
2017-11-23 19:11:05 -0800

    added exercise 18

--- chapter04/exercise18/src/Main.cpp
+++ chapter04/exercise18/src/Main.cpp
@@ -1,7 +1,22 @@
+// solves user's quadratic problem
 #include<iostream>
 #include<cmath>
 
+void quadratic(double a, double b, double c){
+	double radical = pow(b, 2)-(4*a*c);
+	if (radical < 0){
+		std::cout << "error, no real solution\n";
+	}
+	else{
+		std::cout << ((-1 * b) - sqrt(radical))/(2*a) << std::endl;
+		std::cout << ((-1 * b) + sqrt(radical))/(2*a) << std::endl;
+	}
+}
 int main(){
-	std::cout << "ax^2 + bx + c";
+	std::cout << "ax^2 + bx + c = 0" << std::endl;
+	std::cout << "Enter value for a, b, c" << std::endl;
+	double a, b, c;
+	std::cin >> a >> b >> c;
+	quadratic(a, b, c);
 }
 
